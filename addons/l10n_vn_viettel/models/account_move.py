4edbf4102a9985708c7a4298410b8a3774f5c7cc
wan <wan@odoo.com>
2020-01-13 13:59:41 +0100

    [ADD] l10n_vn_viettel: manage invoice serial numbers

---
+++ addons/l10n_vn_viettel/models/account_move.py
@@ -0,0 +1,392 @@
+import json
+import uuid
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+from num2words import num2words
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools import float_compare
+
+import logging
+import urllib3
+from odoo import tools
+
+
+inv_fields_mapping = {
+        "invoiceIssuedDate": "create_date",
+        "invoiceNote": "summary",
+        "buyerName": "customer_name",
+        "buyerLegalName": "customer_legal_name",
+        "buyerTaxCode": "customer_tax_code",
+        "buyerAddressLine": "customer_address",
+        "buyerEmail": "customer_email",
+        "transactionUuid": "transaction_uuid",
+        # seller info
+        "sellerLegalName": "company_name",
+        "sellerTaxCode": "company_tax_code",
+        "sellerAddressLine": "company_address",
+        "sellerPhoneNumber": "company_number",
+        "sellerWebsite": "company_website",
+        "sellerBankAccount": "company_bank_account",
+        "sellerBankName": "company_bank_name",
+
+        "paymentMethodName": "payment_method_name",
+        # summary
+        "sumOfTotalLineAmountWithoutTax": "total_line_amount_wo_tax",
+        "totalAmountWithoutTax": "amount_untaxed",
+        "totalTaxAmount": "amount_tax",
+        "totalAmountWithTax": "amount_total",
+        "totalAmountWithTaxInWords": "amount_total_text",
+        "taxPercentage": "total_tax_percent",
+        "taxBreakdowns": "tax_list",
+}
+
+inv_line_fields_mapping = {
+    "lineNumber": "sequence",
+    "itemCode": "product_ref",
+    "itemName": "product_name",
+    "unitName": "uom_name",
+    "unitPrice": "price_unit",
+    "quantity": "quantity",
+    "itemTotalAmountWithoutTax": "price_subtotal",
+    "taxAmount": "amount_tax",
+    "taxableAmount": "taxable_amount",
+    "taxPercentage": "tax_percent",
+}
+
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    payment_journal_id = fields.Many2one('account.journal', domain="[('type', 'in', ('bank', 'cash')), ('bank_account_id', '!=', False)]", string="Payment Journal", states={'open': [('readonly', True)]})
+    l10n_vn_status = fields.Char(readonly=True, copy=False)
+    l10n_vn_transaction_id = fields.Char(copy=False)
+    l10n_vn_invoice_no = fields.Char(string='Invoice No (Số hóa đơn)', copy=False)
+    l10n_vn_reservation_code = fields.Char(string='Reservation Code (Mã số bí mật)', copy=False)
+    l10n_vn_description = fields.Char(readonly=True, copy=False)
+    l10n_vn_error_code = fields.Char(readonly=True, copy=False)
+    l10n_vn_result_error = fields.Char(readonly=True, copy=False)
+    l10n_vn_uuid = fields.Char(readonly=True, copy=False)
+    l10n_vn_is_success = fields.Boolean(default=False, readonly=True, copy=False)
+    l10n_vn_is_send_mail_success = fields.Boolean(default=False, readonly=True, copy=False)
+    l10n_vn_transaction_date = fields.Char(string='Transaction ID (Ngày lập)', readonly=True, copy=False)
+    l10n_vn_file = fields.Binary(readonly=True, copy=False)
+    l10n_vn_viettel_input = fields.Text(readonly=True)
+    l10n_vn_vas_invoice_date = fields.Datetime(compute='_compute_l10n_vn_vas_invoice_date', store=True)
+    l10n_vn_tax_invoice_number = fields.Char(copy=False)
+    l10n_vn_tax_invoice_series = fields.Char(copy=False)
+
+    @api.onchange('payment_journal_id')
+    def onchange_payment_journal(self):
+        if self.payment_journal_id.bank_account_id:
+            self.invoice_partner_bank_id = self.payment_journal_id.bank_account_id
+
+    def button_export_draft_invoice(self):
+        self.action_export_einvoice(is_draft=True)
+
+    def button_export_confirmed_invoice(self):
+        self.action_export_einvoice(is_draft=False)
+
+    def action_export_einvoice(self, is_draft):
+        self.ensure_one()
+        customer_address = self.partner_id._display_address(without_company=True).replace('\n', ', ')
+        company_address = self.partner_id.company_id.partner_id._display_address(without_company=True).replace('\n', ', ')
+
+        if not customer_address:
+            raise UserError(_("The legal address of partner is not set. Please set it first in order to export!"))
+        vnd_cur = self.env.ref('base.VND')
+        create_date = fields.Datetime.from_string(fields.Datetime.now()).strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+
+        if self.payment_journal_id.type not in ("bank", "cash"):
+            raise UserError(_('The payment journal type should be Bank or Cash'))
+
+        payment_method_name = self.payment_journal_id.type == "bank" and "CK" or "TM"
+        bank_ids = self.env['res.partner.bank'].search([('partner_id', '=', self.env.company.partner_id.id)])
+
+        inv_vals = {
+            'create_date': create_date,
+            # 'summary': self.summary_vn,  # TODO what?
+            'customer_legal_name': self.partner_id.name,
+            'customer_tax_code': self.partner_id.vat or '',
+            'customer_address': customer_address,
+            'customer_email': self.partner_id.email if self.currency_id == vnd_cur else 'accounting@komit-consulting.com',  # TODO unhardcode
+            'transaction_uuid': '%s' % uuid.uuid1(),
+            'company_name': self.company_id.name,
+            'company_tax_code': self.company_id.vat or '',
+            'company_address': company_address,
+            'company_number': self.company_id.phone or '',
+            'company_website': self.company_id.website or '',
+            'company_bank_account': ', '.join(bank_ids.mapped(lambda b: '%s (%s)' % (b.acc_number, (b.currency_id or self.company_id.currency_id).name))),
+            'company_bank_name': ', '.join(bank_ids.mapped('bank_id.name')),
+            'payment_method_name': payment_method_name,
+            'currency_name': self.currency_id.name,
+        }
+
+        inv_lines_vals = []
+        index = 1
+        total_line_amount_wo_tax = 0
+        tax_dict = {}
+        no_tax = self.env.ref('l10n_vn.%s_tax_sale_vat0' % self.company_id.id)
+        for line in self.invoice_line_ids:
+            total_line_amount_wo_tax += line.price_subtotal
+            if not line.tax_ids:
+                raise UserError(_('There is no tax set on line %s') % line.name)
+            # taxPercentage from Viettel: no tax = -2
+            tax_percent = -2 if no_tax in line.tax_ids else line.tax_ids.amount
+
+            if tax_dict.get(tax_percent, False):
+                tax_dict[tax_percent]['taxable_amount'] += line.price_subtotal
+                tax_dict[tax_percent]['tax_amount'] += line.price_total - line.price_subtotal
+            else:
+                tax_dict[tax_percent] = {
+                    'taxable_amount': line.price_subtotal,
+                    'tax_amount': line.price_total - line.price_subtotal,
+                }
+            inv_lines_vals.append({
+                'sequence': index,
+                'product_ref': line.product_id.default_code,
+                'product_name': line.display_name,  # TODO was line.local_description
+                'uom_name': line.product_uom_id.display_name,  # TODO was line.local_name
+                'price_unit': line.price_unit,
+                'quantity': line.quantity,
+                'price_subtotal': line.price_subtotal,
+                'tax_percent': tax_percent,
+                'amount_tax': line.price_total - line.price_subtotal,
+            })
+            index += 1
+
+        tax_list = []
+        for key in tax_dict:
+            tax_list.append({
+                'tax_percent': key,
+                'taxable_amount': tax_dict[key]['taxable_amount'],
+                'amount_tax': tax_dict[key]['tax_amount'],
+            })
+
+        inv_vals.update({
+            'total_line_amount_wo_tax': total_line_amount_wo_tax,
+            'amount_untaxed': self.amount_untaxed,
+            'amount_tax': self.amount_tax,
+            'amount_total': self.amount_total,
+            'amount_total_text': num2words(self.amount_total, lang='vi_VN'),
+            'tax_list': tax_list,
+        })
+
+        data_return, viettel_input = self.l10n_vn_export_to_viettel(inv_vals, inv_lines_vals, is_draft=is_draft)
+        self.write({
+            'l10n_vn_status': data_return['status'],
+            'l10n_vn_description': data_return['description'],
+            'l10n_vn_transaction_id': data_return['transaction_id'],
+            'l10n_vn_invoice_no': data_return['inv_no'],
+            'l10n_vn_reservation_code': data_return['reservation_code'],
+            'l10n_vn_result_error': data_return['result_error'],
+            'l10n_vn_error_code': data_return['error_code'],
+            'l10n_vn_uuid': inv_vals['transaction_uuid'],
+            'l10n_vn_transaction_date': inv_vals['create_date'],
+            'l10n_vn_viettel_input': json.dumps(viettel_input)
+        })
+
+        if self.l10n_vn_status != '200':
+            raise UserError(_(self.l10n_vn_result_error))
+        if self.l10n_vn_status == '200' and data_return['tax_code'] == '':
+            if not is_draft:
+                raise UserError(_('%s\nError code:%s' % (self.l10n_vn_description, self.l10n_vn_error_code)))
+            if is_draft:
+                self.message_post(body="Export Draft E-Invoice successfully!")
+        if self.l10n_vn_status == '200' and self.l10n_vn_invoice_no != '':
+            self.message_post(body="Export E-Invoice to Viettel successfully!")
+            self.l10n_vn_is_success = True
+
+    def action_get_l10n_vn_file(self):
+        self.ensure_one()
+        if not self.l10n_vn_invoice_no:
+            raise UserError(_('The invoice has not been exported to Viettel.\nPlease click to button Export E-Invoice first'))
+        data = self.l10n_vn_get_invoice(self.company_id.vat, self.l10n_vn_invoice_no)[0]
+        if not data['description'] and not data['errorCode']:
+            self.l10n_vn_file = data['fileToBytes']
+        else:
+            raise UserError(_('%s\nError code:%s' % (data['description'], data['errorCode'])))
+
+    def action_send_l10n_vn_mail(self):
+        self.ensure_one()
+        if not self.partner_id.email:
+            raise UserError(_("You cannot send email because you did not specify the customer's email. Please setup email for the customer"))
+        data = self.l10n_vn_send_mail(self.company_id.vat, self.l10n_vn_uuid)
+        data_output = data['commonOutputs']
+        if data_output[0]['description'] == 'SUCCESS':
+            self.message_post(body="Sent E-Invoice mail to customer")
+            self.l10n_vn_is_send_mail_success = True
+        else:
+            raise UserError(_('%s\nError code:%s' % (data_output['description'], data_output['errorCode'])))
+
+    @api.depends('l10n_vn_transaction_date')
+    def _compute_l10n_vn_vas_invoice_date(self):
+        for rec in self:
+            if rec.l10n_vn_transaction_date:
+                vas_date = "".join(list(rec.l10n_vn_transaction_date)[:10])
+                rec.l10n_vn_vas_invoice_date = fields.Datetime.from_string(vas_date)
+            else:
+                rec.l10n_vn_vas_invoice_date = False
+
+    def l10n_nv_request_http(self, body, headers, url_tail, return_dict=True):
+        self.ensure_one()
+        l10n_vn_base_url = self.company_id.l10n_vn_base_url
+        encoded_body = json.dumps(body).encode('utf-8')
+        http = urllib3.PoolManager()
+        headers['Content-Type'] = 'application/json'
+        headers['Accept'] = 'application/json'
+        request_status = 'fail'
+        try:
+            response = http.request('POST', '%s/InvoiceAPI/%s' % (l10n_vn_base_url, url_tail), headers=headers, body=encoded_body)
+            try:
+                json.loads(response.data.decode())
+                if return_dict:
+                    response = json.loads(response.data.decode())
+                request_status = 'success'
+            except json.decoder.JSONDecodeError:
+                response = response.data.decode()
+        except urllib3.exceptions.LocationValueError as e:
+            logging.error(e)
+            logging.info('l10n_vn_base_url: %s', l10n_vn_base_url)
+            logging.info('url_tail: %s', url_tail)
+            logging.info('headers: %s', headers)
+            logging.info('encoded_body: %s', encoded_body)
+            response = e
+        return response, request_status
+
+    def l10n_vn_export_to_viettel(self, inv_vals, inv_lines_vals, is_draft=True):
+        self.ensure_one()
+        inv_lines = []
+        for line in inv_lines_vals:
+            invoice_line = {
+                "lineNumber": line.get(inv_line_fields_mapping['lineNumber']),
+                "itemCode": line.get(inv_line_fields_mapping['itemCode']),
+                "itemName": line.get(inv_line_fields_mapping['itemName']),
+                "unitName": line.get(inv_line_fields_mapping['unitName']),
+                "unitPrice": line.get(inv_line_fields_mapping['unitPrice']),
+                "quantity": line.get(inv_line_fields_mapping['quantity']),
+                "itemTotalAmountWithoutTax": line.get(inv_line_fields_mapping['itemTotalAmountWithoutTax']),
+                "taxPercentage": line.get(inv_line_fields_mapping['taxPercentage']),
+                "taxAmount": line.get(inv_line_fields_mapping['taxAmount']),
+            }
+            inv_lines.append(invoice_line)
+
+        invoice = {
+            "generalInvoiceInfo": {
+                "invoiceType": self.company_id.l10n_vn_type,
+                "templateCode": self.company_id.l10n_vn_template_code,
+                "invoiceSeries": self.company_id.l10n_vn_series,
+                "invoiceIssuedDate": inv_vals.get(inv_fields_mapping['invoiceIssuedDate']),
+                "currencyCode": inv_vals.get('currency_name'),
+                "adjustmentType": "1",
+                "paymentStatus": True,
+                "paymentType": "CK",
+                "paymentTypeName": "CK",
+                "cusGetInvoiceRight": True,
+                "transactionUuid": inv_vals.get(inv_fields_mapping['transactionUuid']),
+            },
+            "buyerInfo": {
+                "buyerName": "",
+                "buyerLegalName": inv_vals.get(inv_fields_mapping['buyerLegalName']),
+                "buyerTaxCode": inv_vals.get(inv_fields_mapping['buyerTaxCode']),
+                "buyerAddressLine": inv_vals.get(inv_fields_mapping['buyerAddressLine']),
+                "buyerEmail": inv_vals.get(inv_fields_mapping['buyerEmail'])
+            },
+            "sellerInfo": {
+                "sellerLegalName": inv_vals.get(inv_fields_mapping['sellerLegalName']),
+                "sellerTaxCode": inv_vals.get(inv_fields_mapping['sellerTaxCode']),
+                "sellerAddressLine": inv_vals.get(inv_fields_mapping['sellerAddressLine']),
+                "sellerPhoneNumber": inv_vals.get(inv_fields_mapping['sellerPhoneNumber']),
+                "sellerWebsite": inv_vals.get(inv_fields_mapping['sellerWebsite']),
+                "sellerBankAccount": inv_vals.get(inv_fields_mapping['sellerBankAccount']),
+                "sellerBankName": inv_vals.get(inv_fields_mapping['sellerBankName']),
+            },
+            "payments": [
+                {
+                    "paymentMethodName": inv_vals.get(inv_fields_mapping['paymentMethodName']),
+                }
+            ],
+            "itemInfo": inv_lines,
+            "summarizeInfo": {
+                "sumOfTotalLineAmountWithoutTax": inv_vals.get(inv_fields_mapping['sumOfTotalLineAmountWithoutTax']),
+                "totalAmountWithoutTax": inv_vals.get(inv_fields_mapping['totalAmountWithoutTax']),
+                "totalTaxAmount": inv_vals.get(inv_fields_mapping['totalTaxAmount']),
+                "totalAmountWithTax": inv_vals.get(inv_fields_mapping['totalAmountWithTax']),
+                "totalAmountWithTaxInWords": inv_vals.get(inv_fields_mapping['totalAmountWithTaxInWords']),
+                "discountAmount": 0.0,
+            },
+            "taxBreakdowns": [
+                {
+                    "taxPercentage": item.get(inv_line_fields_mapping['taxPercentage']),
+                    "taxableAmount": item.get(inv_line_fields_mapping['taxableAmount']),
+                    "taxAmount": item.get(inv_line_fields_mapping['taxAmount']),
+                } for item in inv_vals.get(inv_fields_mapping['taxBreakdowns'])
+            ],
+        }
+        headers = urllib3.util.make_headers(basic_auth=self.company_id.l10n_vn_authority)
+        create_status = 'createOrUpdateInvoiceDraft' if is_draft else 'createInvoice'
+        url_tail = 'InvoiceWS/{0}/{1}'.format(create_status, inv_vals.get('company_tax_code'))
+        r, request_status = self.l10n_nv_request_http(invoice, headers, url_tail, return_dict=False)
+
+        if request_status == 'fail':
+            result = {
+                'status': 'Connection error',
+                'description': 'N/A',
+                'tax_code': 'N/A',
+                'transaction_id': 'N/A',
+                'inv_no': 'N/A',
+                'reservation_code': 'N/A',
+                'error_code': 'K500',
+                'result_error': r,
+            }
+            return result, invoice
+
+        data_dict = json.loads(r.data.decode())
+        data_description = data_dict['description']
+        data_tax_code = data_tran_id = data_inv_no = data_reservation_code = data_result_error = ''
+        if isinstance(data_dict['result'], dict):
+            data_tax_code = data_dict['result'].get('supplierTaxCode')
+            data_tran_id = data_dict['result'].get('transactionID')
+            data_inv_no = data_dict['result'].get('invoiceNo', "")
+            data_reservation_code = data_dict['result'].get('reservationCode')
+        else:
+            data_result_error = data_dict['result']
+
+        data_error_code = data_dict['errorCode']
+
+        result = {
+            'status': r.status,
+            'description': data_description,
+            'tax_code': data_tax_code,
+            'transaction_id': data_tran_id,
+            'inv_no': data_inv_no,
+            'reservation_code': data_reservation_code,
+            'error_code': data_error_code,
+            'result_error': data_result_error
+        }
+        return result, invoice
+
+    def l10n_vn_get_invoice(self, tax_code, inv_no):
+        self.ensure_one()
+        body = {
+            "supplierTaxCode": tax_code,
+            "invoiceNo": inv_no,
+            "pattern": self.company_id.l10n_vn_template_code,
+            "fileType": "PDF",
+        }
+
+        headers = urllib3.util.make_headers(basic_auth=self.company_id.l10n_vn_authority)
+        url_tail = 'InvoiceUtilsWS/getInvoiceRepresentationFile'
+        return self.l10n_nv_request_http(body, headers, url_tail)
+
+    def l10n_vn_send_mail(self, tax_code, uuid):
+        self.ensure_one()
+        body = {
+            "supplierTaxCode": tax_code,
+            "lstTransactionUuid": uuid,
+        }
+        l10n_vn_auth = self.company_id.l10n_vn_authority
+        headers = urllib3.util.make_headers(basic_auth=l10n_vn_auth)
+        url_tail = 'InvoiceUtilsWS/sendHtmlMailProcess'
+        return self.l10n_nv_request_http(body, headers, url_tail)
