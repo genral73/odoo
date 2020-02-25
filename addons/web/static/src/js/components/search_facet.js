bf9aa4b7d2bc6444361eb5cab6ff6881066709d5
Mathieu Duckerts-Antoine <dam@odoo.com>
2020-02-25 16:03:26 +0100

    [REF] SearchFacet: work on descriptions

--- addons/web/static/src/js/components/search_facet.js
+++ addons/web/static/src/js/components/search_facet.js
@@ -35,75 +35,9 @@ class SearchFacet extends Component {
     get values() {
         const filters = Object.values(this.props.filters);
         if (this.props.group.type === 'field') {
-            return filters.reduce(
-                (filters, filter) => [...filters, ...filter.autoCompleteValues.map(acv => acv.label)],
-                []
-            );
-        } else {
-            return filters.map(filter => this._getFilterDescription(filter));
+            return filters[0].descriptionInFacet;
         }
-    }
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * Get the correct description according to filter.
-     *
-     * @private
-     * @returns {string}
-     */
-    _getFilterDescription(filter) {
-        if (filter.type === 'timeRange') {
-            let description = `${filter.fieldDescription}: ${filter.rangeDescription}`;
-            if (filter.comparisonRangeDescription) {
-                description += ` / ${filter.comparisonRangeDescription}`;
-            }
-            return description;
-        }
-
-        let description = filter.description;
-        if (filter.hasOptions) {
-
-            const currentOptions = filter.options.filter(o => o.isActive);
-            const descriptions = [];
-
-            if (filter.type === 'filter') {
-                const unsortedYearIds = [];
-                const unsortedOtherOptionIds = [];
-                currentOptions.forEach(o => {
-                    if (o.groupNumber === 2) {
-                        unsortedYearIds.push(o.optionId);
-                    } else {
-                        unsortedOtherOptionIds.push(o.optionId);
-                    }
-                });
-                const sortOptionIds = (a, b) =>
-                    filter.options.findIndex(({ optionId }) => optionId === a) -
-                    filter.options.findIndex(({ optionId }) => optionId === b);
-
-                const yearIds = unsortedYearIds.sort(sortOptionIds);
-                const otherOptionIds = unsortedOtherOptionIds.sort(sortOptionIds);
-
-                if (otherOptionIds.length) {
-                    otherOptionIds.forEach(optionId => {
-                        yearIds.forEach(yearId => {
-                            descriptions.push(filter.basicDomains[`${yearId}__${optionId}`].description);
-                        });
-                    });
-                } else {
-                    yearIds.forEach(yearId => {
-                        descriptions.push(filter.basicDomains[yearId].description);
-                    });
-                }
-            } else {
-                descriptions.push(...currentOptions.map(o => o.description));
-            }
-            description += `: ${descriptions.join(" / ")}`;
-        }
-        return description;
-
+        return filters.map(filter => filter.descriptionInFacet);
     }
 
     //--------------------------------------------------------------------------
