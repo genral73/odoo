9ba801b21cab6b4c984e3bd385a2fc6bfae20e3f
Ipsita Borisagar <ibo@odoo.com>
2020-03-03 12:12:50 +0530

    [IMP] mail_bot: improve preferences warning
    Before this commit, once the push notifications have been blocked, message in
    the preferences is not straightforward enough for users to allow them.
    
    After this commit, user can easily copy current browser setting's URL by copy
    button in the message.
    
    PR https://github.com/odoo/odoo/pull/47919
    Task-2188584

--- addons/mail_bot/static/src/js/notification_alert.js
+++ addons/mail_bot/static/src/js/notification_alert.js
@@ -8,15 +8,51 @@ var widgetRegistry = require('web.widget_registry');
 // Display Notification alert on user preferences form view
 // -----------------------------------------------------------------------------
 var NotificationAlert = Widget.extend({
-   template: 'mail.NotificationAlert',
-   /**
-    * @override
-    */
-   init: function () {
-      this._super.apply(this, arguments);
-      var hasRequest = this.call('mailbot_service', 'isRequestingForNativeNotifications');
-      this.isNotificationBlocked = window.Notification && window.Notification.permission !== "granted" && !hasRequest;
-   },
+    template: 'mail.NotificationAlert',
+    /**
+     * @override
+     */
+    init: function () {
+        this._super.apply(this, arguments);
+        var hasRequest = this.call('mailbot_service', 'isRequestingForNativeNotifications');
+        this.isNotificationBlocked = window.Notification && window.Notification.permission !== "granted" && !hasRequest;
+        if (this.isNotificationBlocked) {
+            if (navigator.userAgent.search("Chrome") != -1) {
+                this.settingUrl = "chrome://settings/content/siteDetails?site=https%3A%2F%2Fdatabasename.odoo.com";
+            } else if (navigator.userAgent.search("Firefox") != -1) {
+                this.settingUrl = "about:preferences#privacy";
+            } else if (navigator.userAgent.search("Safari") != -1) {
+                this.settingPath = "(path:Safari > Preferences > Website > Notifications)";
+            }
+        }
+    },
+    /**
+     * Copies the browser settings URL link to the clipboard.
+     *
+     * @override
+     */
+    start: function() {
+        this._super.apply(this, arguments);
+        var self = this;
+        if (this.isNotificationBlocked && this.settingUrl) {
+            var $clipboardBtn = this.$('.o_clipboard_button');
+            $clipboardBtn.tooltip({title: 'Copied !', trigger: "manual", placement: "right"});
+            var clipboard = new ClipboardJS($clipboardBtn[0], {
+                text: function () { return self.settingUrl; },
+                container: this.el,
+            });
+            clipboard.on("success", function (e) {
+                clipboard.destroy();
+                _.defer(function () {
+                    $clipboardBtn.tooltip("show");
+                    _.delay(function () { $clipboardBtn.tooltip("hide"); }, 800);
+                });
+            });
+            clipboard.on('error', function (e) {
+                clipboard.destroy();
+            });
+        }
+    },
 });
 
 widgetRegistry.add('notification_alert', NotificationAlert);
