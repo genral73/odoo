7607710b9982c4b2812733f04c824ff88e8a34b6
Miku Laitinen <miku.laitinen@avoin.systems>
2020-03-16 18:43:22 +0100

    [ADD] Added l10n_fi without CoA

--- addons/l10n_fi/__manifest__.py
+++ addons/l10n_fi/__manifest__.py
@@ -9,21 +9,23 @@
 ##############################################################################
 
 {
-    "name" : "Finland - Accounting",
-    "version" : "12.0.0",
-    "author" : "Sprintit",
-    "category" : "Localization/Account Charts",
-    "description": """This is the module to manage the accounting chart for Finland in Odoo.
-
-When installed on a system without any chart of accounts, this module creates following accounting data localzied for Finland:
-Chart of accounts (if COA is not defined yet), Taxes, Tax tags (used in l10n_fi_reports), Fiscal positions, Account types (used in l10n_fi_reports)
-""",
+    "name": "Finnish Localization",
+    "version": "13.0.1",
+    "author": "Sprintit",
+    "author": "Avoin.Systems, "
+              "Tawasta, "
+              "Vizucom, "
+              "Sprintit",
+    "category": "Localization",
+    "description": "This is the Odoo module to manage the accounting in Finland.",
     "depends": [
         'account',
         'base_iban',
         'base_vat',
     ],
     "data": [
+        "data/res_partner_operator_einvoice_data.xml",
+        "security/ir.model.access.csv",
         'data/account_account_tag_data.xml',
         'data/account_chart_template_data.xml',
         'data/account.account.template.csv',
@@ -32,7 +34,11 @@ Chart of accounts (if COA is not defined yet), Taxes, Tax tags (used in l10n_fi_
         'data/l10n_fi_chart_post_data.xml',
         'data/account_fiscal_position_template_data.xml',
         'data/account_chart_template_configuration_data.xml',
-
+        "views/menuitems.xml",
+        "views/res_company_views.xml",
+        "views/res_config_settings_views.xml",
+        "views/res_partner_operator_einvoice_views.xml",
+        "views/res_partner_views.xml",
     ],
     "active": True,
     "installable": True,
