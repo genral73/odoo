7bb7466dd8b4e7127cb3f519641f906350cadf94
jpr-odoo <jpr@openerp.com>
2020-03-20 10:50:04 +0530

    update carousel indicators

--- addons/website_sale/static/src/js/website_sale_product_variant_image.js
+++ addons/website_sale/static/src/js/website_sale_product_variant_image.js
@@ -1,17 +1,28 @@
 odoo.define('website_sale.product_variant_image', function (require) {
 "use strict";
 
-// var core = require('web.core');
+var core = require('web.core');
 var Widget = require('web.Widget');
-var MediaDialog = require('wysiwyg.widgets').MediaDialog;
+var weWidgets = require('wysiwyg.widgets');
 
-// var _t = core._t;
-// var QWeb = core.qweb;
+var _t = core._t;
+var QWeb = core.qweb;
 
 var productVariantImages = Widget.extend({
-
+    xmlDependencies: ['/website_sale/static/src/xml/website_sale.xml'],
     events: {
         'click': '_onClick',
+    },
+
+    /**
+     * @override
+     */
+    start: function () {
+        const def = this._super.apply(this, arguments);
+        this.productVariantImages = [];
+        this.currentIndex = this.$el.data('slide-to');
+        this.productID = this.$el.data('product-id');
+        return def;
     },
 
     //--------------------------------------------------------------------------
@@ -22,12 +33,33 @@ var productVariantImages = Widget.extend({
      * @private
      */
     _onClick: function (ev) {
-        var $image = $("<img/>");
-        var mediaDialog = new MediaDialog(this, {
-            noIcons: true,
-            noDocuments: true,
-        },$image[0]);
-        mediaDialog.open();
+        debugger;
+        const self = this;
+        const dialog = new weWidgets.Dialog(this, {
+            size: 'large',
+            title: _t(" Extra Product Media"),
+            $content: $(QWeb.render('productImageVideoUploder', {})),
+            buttons: [{
+                text: _t("Add"),
+                classes: 'btn-primary',
+                click: function () {
+                    let data = {
+                        fields: this.$content.find('input'),
+                        image: this.$content.find('img'),
+                    };
+                    self._updateCarouselIndicator(ev, data);
+                }, close: true
+            },{text: _t("Cancel"), close: true,}],
+        });
+        dialog.open();
+    },
+    _updateCarouselIndicator: function (ev, data) {
+        let $newli = $("<li class='d-inline-block m-1 align-top o_wsale_product_new_img' data-target='#o-carousel-product'/>").attr('data-slide-to', this.currentIndex);
+        this.currentIndex = this.currentIndex + 1;
+        this.$el.attr('data-slide-to', this.currentIndex);
+        data.image.appendTo($newli);
+        $newli.insertBefore(this.$el);
+        this.productVariantImages.push(data);
     },
 });
 return productVariantImages;
