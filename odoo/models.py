yf956613/odoo-1

[IMP] fields: add validation of parameter names

odoo/models.py
@@ -282,6 +282,10 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
         self.env['ir.model.constraint']._reflect_model(self)
         self.invalidate_cache()
 
+    def _valid_field_parameter(self, field, name):
+        """ Return whether the given parameter name is valid for the field. """
+        return name == 'oldname'
+
     @api.model
     def _add_field(self, name, field):
         """ Add the given ``field`` under the given ``name`` in the class """

genral73/odoo

odoo/models.py
@@ -363,6 +363,10 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
         self.env['ir.model.constraint']._reflect_model(self)
         self.invalidate_cache()
 
+    def _valid_field_parameter(self, field, name):
+        """ Return whether the given parameter name is valid for the field. """
+        return name == 'related_sudo'
+
     @api.model
     def _add_field(self, name, field):
         """ Add the given ``field`` under the given ``name`` in the class """