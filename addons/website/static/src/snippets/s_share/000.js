1b08213d703115199802b572f8a764f60ba3fc4f
bbh-odoo <bbh@odoo.com>
2020-03-25 19:33:17 +0530

    [IMP] website,website_sale: pinterest,what'sapp url change

--- addons/website/static/src/snippets/s_share/000.js
+++ addons/website/static/src/snippets/s_share/000.js
@@ -11,19 +11,21 @@ const ShareWidget = publicWidget.Widget.extend({
      */
     start: function () {
         var urlRegex = /(\?(?:|.*&)(?:u|url|body)=)(.*?)(&|#|$)/;
-        var titleRegex = /(\?(?:|.*&)(?:title|text|subject)=)(.*?)(&|#|$)/;
+        var titleRegex = /(\?(?:|.*&)(?:title|text|subject|description)=)(.*?)(&|#|$)/;
+        var mediaRegex = /(\?(?:|.*&)(?:media)=)(.*?)(&|#|$)/;
         this.$('a').each(function () {
             var $a = $(this);
             if (($a.attr('target') && $a.attr('target').match(/_blank|_self/i) && !$a.closest('.o_editable').length)) {
                 $a.on('click', function () {
-                    let url = encodeURIComponent(window.location.href),title = encodeURIComponent($('title').text());
+                    let url = encodeURIComponent(window.location.href),title = encodeURIComponent($('title').text()),media = encodeURIComponent(window.location.origin + $('.product_detail_img').attr('src'));
                      $a.attr('href', function (i, href) {
                         return href.replace(urlRegex, function (match, a, b, c) {
-                            debugger
                             return a + url + c;
                         })
                         .replace(titleRegex, function (match, a, b, c) {
-                            return a + title + c;
+                            return ($a.hasClass('s_share_whatsapp')? a + title +url+ c : a + title + c);
+                        }).replace(mediaRegex,function(match, a, b, c){
+                            return a + media + c;
                         });
                     });
                     window.open(this.href, this.target, 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=550,width=600');
