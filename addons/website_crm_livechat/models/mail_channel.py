dc5ca0e27c83c46293a5ab30ecade7b5b930ca01
Dharmraj Jhala <dja@odoo.com>
2020-03-26 16:13:36 +0530

    review

--- addons/website_crm_livechat/models/mail_channel.py
+++ addons/website_crm_livechat/models/mail_channel.py
@@ -16,16 +16,21 @@ class MailChannel(models.Model):
         visitor_sudo = self.livechat_visitor_id.sudo()
         if visitor_sudo:
             visitor_sudo.write({'lead_ids': [(4, lead.id)]})
-            visited_url = self.env['website.track'].search(
-                [('visitor_id', '=', visitor_sudo.id)]).filtered(lambda track: 'utm_campaign' in track.url).mapped('url')
-            utm_data = dict(parse.parse_qsl(parse.urlsplit(visited_url[0]).query)) if visited_url else False
+            utm_page_view = self.env['website.track'].search([
+                '&', ('visitor_id', '=', visitor_sudo.id),
+                '|', '|',
+                ('url', 'like', '%%utm_campaign%%'),
+                ('url', 'like', '%%utm_source%%'),
+                ('url', 'like', '%%utm_medium%%')
+            ], limit=1, order='visit_datetime')
+            utm_data = dict(parse.parse_qsl(parse.urlsplit(utm_page_view.url).query)) if utm_page_view else False
             if utm_data:
                 campaign_id = self.env['utm.campaign'].search([('name', '=', utm_data.get('utm_campaign'))])
                 source_id = self.env['utm.source'].search([('name', '=', utm_data.get('utm_source'))])
                 medium_id = self.env['utm.medium'].search([('name', '=', utm_data.get('utm_medium'))])
                 lead.write({
-                    'campaign_id': campaign_id and campaign_id.id or lead.campaign_id,
-                    'medium_id': medium_id and medium_id.id or lead.medium_id,
-                    'source_id': source_id and source_id.id or lead.source_id
+                    'campaign_id': campaign_id.id or lead.campaign_id.id,
+                    'medium_id': medium_id.id or lead.medium_id.id,
+                    'source_id': source_id.id or lead.source_id.id
                 })
         return lead
